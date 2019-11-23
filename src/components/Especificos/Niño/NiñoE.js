00322106787c26859aca7928fcb2d24e1631ecd2
panch2203 <panch2203@gmail.com>
2019-11-23 00:28:32 -0600

    como imprimir los valores jaja

--- src/components/Especificos/Niño/NiñoE.js
+++ src/components/Especificos/Niño/NiñoE.js
@@ -10,22 +10,47 @@ import Pencil from '../../../assets/pencil.jpg';
 class FullPost extends Component {
     state={
         status: true,
-        loadedPost:null
+        loadedPost:null,
+        ninoRegalo:[],
+        ninoEspecifico:[""]
     }
 
     changeStatusHandler=()=>{
         this.setState({status:!this.state.status});
     }
 
+
+
     componentDidMount(){
 
             axios.get("https://api-mongod.herokuapp.com/ninos/"+this.props.match.params.id)
             .then(response=>{
-              console.log(response);
-            this.setState({loadedPost:response.data.nino});
-            this.setState({status:response.data.nino.status})
-            });
+              this.setState({loadedPost:response.data.nino});
+              this.setState({status:response.data.nino.status})
+            })
+            .catch(e => console.log(e));
+
+            axios.get("https://api-mongod.herokuapp.com/ninosregalos")
+            .then(response=>{
+              this.setState({ninoRegalo:response.data.result});
+              console.log("array de los niños")
+              console.log(this.state.ninoRegalo);
+              const ninoEsp = response.data.result.filter( x => x.idNino === this.props.match.params.id )
+              .map(function( obj ) {
+                return obj.idRegalo;
+              });
+
+              this.setState({ninoEspecifico:ninoEsp});
+            })
+            .catch(e => console.log(e));
+
+
         }
+
+      componentDidUpdate() {
+        console.log("prueba");
+        console.log(this.state.ninoEspecifico);
+      }
 
     deletePostHandler=()=>{
 
@@ -53,8 +78,26 @@ class FullPost extends Component {
         console.log("Voy a agregar un regalo");
     }
 
+    nombreRegalo=(x)=>{
+      console.log("probando metodo id")
+      console.log(x);
+      let valor = "";
+      axios.get("https://api-mongod.herokuapp.com/regalos/"+x)
+      .then(response=>{
+        console.log(response.data.regalo);
+        valor = response.data.regalo.nombre;
+        console.log("ahora el nombre");
+        console.log(valor);
+        return valor;
+
+      })
+      .catch(e => console.log(e));
+      console.log(valor)
+
+
+    }
+
     render () {
-      console.log(this.state.loadedPost);
 
       console.log(this.props.match.params.id);
         let y;
@@ -65,7 +108,6 @@ class FullPost extends Component {
         }
         let st;
         if (this.loadedPost){
-            console.log(this.loadedPost.nombre);
             if (this.loadedPost.genero === true){
                 st="niño";
             }
@@ -74,6 +116,9 @@ class FullPost extends Component {
 
         let post = <p style={{textAlign:'center'}}>Please select a Post!</p>;
         let v=new Date();
+
+
+
         if (this.props.id){
             post = <p style={{textAlign:'center'}}>Loading...</p>;
         }
@@ -104,6 +149,10 @@ class FullPost extends Component {
                     <div className="regaloInfoE">
                         <div className="info">
                             <p>Regalos</p>
+                            <ul>
+                              {this.state.ninoEspecifico.map((x) => <li>{this.nombreRegalo(x)}</li>)}
+
+                            </ul>
                             <p>Costo total: 98765.00 $</p>
                             <button className="addButton" onClick={this.addRegalo}>+</button>
                         </div>
